0295042b6a6bc0b1dc95d2dce8171b5602512040
I-Novotek Coding Institute <emmanuel@I-Novoteks-iMac.local>
2023-05-12 17:08:38 -0700

    Rendering Homepage compone

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./components/Homepage/Homepage";
 
 export default function App() {
-  const HomePage = () => {
-    return <h1>Welcome to Blogify</h1>;
-  };
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<HomePage />}></Route>
+        <Route path="/login" element={<Homepage />}></Route>
       </Routes>
     </BrowserRouter>
   );
