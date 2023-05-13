937d14415e68f94131b5831e6eceb70accf4fafd
I-Novotek Coding Institute <emmanuel@I-Novoteks-iMac.local>
2023-05-12 17:35:23 -0700

    added login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
+import Login from "./components/Users/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Homepage />}></Route>
+        <Route path="" element={<Homepage />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   );
