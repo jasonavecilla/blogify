81a4047ad27480a953c9d90607805f62902fec81
I-Novotek Coding Institute <emmanuel@192.168.0.101>
2023-05-15 04:37:14 -0700

    handle Image form input

--- src/components/Posts/AddPost.js
+++ src/components/Posts/AddPost.js
@@ -26,18 +26,17 @@ const AddPost = () => {
     category: null,
     content: "",
   });
-  // Dummy categories
-  // const options = [
-  //   { value: "technology", label: "Technology" },
-  //   { value: "business", label: "Business" },
-  //   { value: "lifestyle", label: "Lifestyle" },
-  // ];
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
   //! React select handle change
   const handleSelectChange = (selectedOption) => {
     setFormData({ ...formData, category: selectedOption.value });
+  };
+  //! Handle image change
+  const handleFileChange = (e) => {
+    setFormData({ ...formData, image: e.target.files[0] });
   };
   const handleSubmit = (e) => {
     console.log(formData);
@@ -78,6 +77,7 @@ const AddPost = () => {
               className="py-3 px-3 leading-5 w-full text-coolGray-400 font-normal border border-coolGray-200 outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-lg shadow-sm"
               type="file"
               name="image"
+              onChange={handleFileChange}
             />
             {/* error here */}
           </label>
