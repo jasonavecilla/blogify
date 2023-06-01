e6cc09ed47047457ca00996da6baef62c0479906
I-Novotek Coding Institute <emmanuel@I-Novoteks-iMac.local>
2023-06-01 04:10:11 -0700

    Upload profile and Cover Actions

--- src/redux/slices/users/usersSlices.js
+++ src/redux/slices/users/usersSlices.js
@@ -206,6 +206,59 @@ export const logoutAction = createAsyncThunk("users/logout", async () => {
   localStorage.removeItem("userInfo");
   return true;
 });
+// ! upload cover image
+export const uploadCoverImageAction = createAsyncThunk(
+  "users/upload-cover-image",
+  async (payload, { rejectWithValue, getState, dispatch }) => {
+    try {
+      //convert the payload to formdata
+      const formData = new FormData();
+      formData.append("file", payload?.image);
+
+      const token = getState().users?.userAuth?.userInfo?.token;
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const { data } = await axios.post(
+        "http://localhost:9080/api/v1/users/upload-cover-image",
+        formData,
+        config
+      );
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
+// ! upload profile image
+export const uploadProfileImageAction = createAsyncThunk(
+  "users/upload-profile-image",
+  async (payload, { rejectWithValue, getState, dispatch }) => {
+    try {
+      //convert the payload to formdata
+      const formData = new FormData();
+      formData.append("file", payload?.image);
+      const token = getState().users?.userAuth?.userInfo?.token;
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const { data } = await axios.post(
+        "http://localhost:9080/api/v1/users/upload-profile-image",
+        formData,
+        config
+      );
+      return data;
+    } catch (error) {
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
 //! Users slices
 const usersSlice = createSlice({
   name: "users",
@@ -237,6 +290,35 @@ const usersSlice = createSlice({
       state.error = null;
     });
     builder.addCase(userPublicProfileAction.rejected, (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    });
+
+    //upload user profile image
+    builder.addCase(uploadProfileImageAction.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(uploadProfileImageAction.fulfilled, (state, action) => {
+      state.profile = action.payload;
+      state.success = true;
+      state.loading = false;
+      state.error = null;
+    });
+    builder.addCase(uploadProfileImageAction.rejected, (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    });
+    //! upload user cover image
+    builder.addCase(uploadCoverImageAction.pending, (state, action) => {
+      state.loading = true;
+    });
+    builder.addCase(uploadCoverImageAction.fulfilled, (state, action) => {
+      state.profile = action.payload;
+      state.success = true;
+      state.loading = false;
+      state.error = null;
+    });
+    builder.addCase(uploadCoverImageAction.rejected, (state, action) => {
       state.error = action.payload;
       state.loading = false;
     });
