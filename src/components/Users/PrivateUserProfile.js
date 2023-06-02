5c0d120c000a30a168669e97b87a149a6e9a2688
I-Novotek Coding Institute <emmanuel@192.168.0.102>
2023-06-02 03:14:04 -0700

    Show warning if user account is not verified-Frontend

--- src/components/Users/PrivateUserProfile.js
+++ src/components/Users/PrivateUserProfile.js
@@ -1,31 +1,12 @@
 import { AiFillCamera } from "react-icons/ai";
+import { ExclamationTriangleIcon } from "@heroicons/react/20/solid";
+
 import { userPrivateProfileAction } from "../../redux/slices/users/usersSlices";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import UserPosts from "./UserPosts";
 import Followers from "./Followers";
 import { Link } from "react-router-dom";
-const profile = {
-  name: "Ricardo Cooper",
-  imageUrl:
-    "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-  coverImageUrl:
-    "https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-  about: `
-    <p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
-    <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>
-  `,
-  fields: {
-    Phone: "(555) 123-4567",
-    Email: "ricardocooper@example.com",
-    Title: "Senior Front-End Developer",
-    Team: "Product Development",
-    Location: "San Francisco",
-    Sits: "Oasis, 4th floor",
-    Salary: "$145,000",
-    Birthday: "June 8, 1990",
-  },
-};
 
 export default function PrivateUserProfile() {
   //! Get data from store
@@ -35,7 +16,7 @@ export default function PrivateUserProfile() {
     dispatch(userPrivateProfileAction());
   }, [dispatch]);
 
-  const { user, loading, error, profile } = useSelector(
+  const { user, loading, error, profile, userAuth } = useSelector(
     (state) => state?.users
   );
 
@@ -92,6 +73,25 @@ export default function PrivateUserProfile() {
                             {profile?.user?.username}
                           </h1>
                         </div>
+                        {/* Warning */}
+
+                        {!userAuth?.userInfo?.isVerified && (
+                          <button className="rounded-md mt-6 bg-yellow-50 p-4">
+                            <div className="flex">
+                              <div className="flex-shrink-0">
+                                <ExclamationTriangleIcon
+                                  className="h-5 w-5 text-yellow-400"
+                                  aria-hidden="true"
+                                />
+                              </div>
+                              <div className="ml-3">
+                                <h3 className="text-sm font-medium text-yellow-800">
+                                  Click here to verify your account
+                                </h3>
+                              </div>
+                            </div>
+                          </button>
+                        )}
                         <div className="flex flex-col mt-6 space-y-3 justify-stretch sm:flex-row sm:space-y-0 sm:space-x-4">
                           {/* Profile Views */}
                           <button
