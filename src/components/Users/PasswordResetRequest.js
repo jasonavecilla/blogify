32bd2281630b847109f51f9237efcc278987d6c1
I-Novotek Coding Institute <emmanuel@I-Novoteks-iMac.local>
2023-06-02 16:51:12 -0700

    Dispatch password Resset Request Acrion

--- src/components/Users/PasswordResetRequest.js
+++ src/components/Users/PasswordResetRequest.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineMail } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { forgotPasswordAction } from "../../redux/slices/users/usersSlices";
 
 const PasswordResetRequest = () => {
+  //! Dispatch
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    email: "",
+  });
+  //handle form change
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  //handle form submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //!dispatch
+    dispatch(
+      forgotPasswordAction({
+        email: formData.email,
+      })
+    );
+    // reset form
+    setFormData({
+      email: "",
+    });
+  };
+  //store data
+  const { loading, error, isEmailSent } = useSelector((state) => state?.users);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center justify-center min-h-screen bg-gray-50"
+    >
       <div className="w-96 p-6 bg-white rounded-xl shadow-md">
         <h1 className="text-3xl font-bold text-gray-700 text-center mb-6">
           Reset your password
@@ -15,6 +45,9 @@ const PasswordResetRequest = () => {
         <div className="mb-6 relative">
           <AiOutlineMail className="absolute text-gray-500 text-2xl top-2 left-2" />
           <input
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
             type="email"
             placeholder="Enter your email"
             className="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
@@ -24,7 +57,7 @@ const PasswordResetRequest = () => {
           Send reset link
         </button>
       </div>
-    </div>
+    </form>
   );
 };
 
